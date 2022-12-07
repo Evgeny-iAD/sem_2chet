381c30bf0c12eceebad545ae88ee5d50bd8b4f5f
iAD <iadmail1987@gmail.com>
2022-12-08 00:12:10 +0300

    В путь!

--- dz_1.py
+++ dz_1.py
@@ -1,16 +1,104 @@
-# This is a sample Python script.
+# 1. Напишите программу, которая принимает на вход цифру, обозначающую день недели, и проверяет, является ли этот день выходным.
 
-# Press Shift+F10 to execute it or replace it with your code.
-# Press Double Shift to search everywhere for classes, files, tool windows, actions, and settings.
+# num_den = int(input('Введите число дня недели: '))
+#
+# for i in range(1, 8, 1):
+#     if i == num_den:
+#         if num_den == 6 or num_den == 7:
+#             print('Да')
+#         else:
+#             print('Нет')
 
+# 2. Напишите программу для проверки истинности утверждения ¬(X ⋁ Y ⋁ Z) = ¬X ⋀ ¬Y ⋀ ¬Z для всех значений предикат.
+#
+# def tabl1():
+#     print('\nВыражение 1:')
+#     stroka = ''
+#     for x in range(0, 2):
+#         for y in range(0, 2):
+#             for z in range(0, 2):
+#                 print(f'{x}  {y}  {z}  ¬(X⋁Y⋁Z) = {not_disjunctio(x, y, z)}')
+#                 stroka = stroka + str(not_disjunctio(x, y, z))
+#     return stroka
+#
+# def tabl2():
+#     print('\nВыражение 2:')
+#     stroka = ''
+#     for x in range(0, 2):
+#         for y in range(0, 2):
+#             for z in range(0, 2):
+#                 print(f'{x}  {y}  {z}  X⋀Y⋀Z = {conjunctio_not(x, y, z)}')
+#                 stroka = stroka + str(conjunctio_not(x, y, z))
+#     return stroka
+#
+# def not_disjunctio(x, y, z):
+#     if x == 1 or y == 1 or z == 1:
+#         return 0
+#     else:
+#         return 1
+#
+# def inversion(a):
+#     if a == 0:
+#         return 1
+#     else:
+#         return 0
+# def conjunctio_not(x, y, z):
+#     if inversion(x) == 1 and inversion(y) == 1 and inversion(z) == 1:
+#         return 1
+#     else:
+#         return 0
+#
+# if tabl1() == tabl2():
+#     print('\nВыражение ¬(X ⋁ Y ⋁ Z) = ¬X ⋀ ¬Y ⋀ ¬Z  истино')
+# else:
+#     print('\nВыражение ¬(X ⋁ Y ⋁ Z) = ¬X ⋀ ¬Y ⋀ ¬Z  лож')
 
-def print_hi(name):
-    # Use a breakpoint in the code line below to debug your script.
-    print(f'Hi, {name}')  # Press Ctrl+F8 to toggle the breakpoint.
+# 3.Напишите программу, которая принимает на вход координаты точки (X и Y), причём X ≠ 0 и Y ≠ 0 и выдаёт номер
+# четверти плоскости, в которой находится эта точка (или на какой оси она находится)
+#
+# def search_quarter(x, y):
+#     return {
+#             x > 0 and y > 0: 'Первая четверть',
+#             x < 0 and y > 0: 'Вторая четверть',
+#             x < 0 and y < 0: 'Третья четверть',
+#             x > 0 and y < 0: 'Четвертая четверть'
+#             }[True]
+#
+# x = int(input('Введите координату Х: '))
+# y = int(input('Введите координату Y: '))
+#
+# if x != 0 and y != 0:
+#     print(search_quarter(x, y))
 
+# 4. Напишите программу, которая по заданному номеру четверти, показывает диапазон возможных координат точек в этой четверти (x и y).
 
-# Press the green button in the gutter to run the script.
-if __name__ == '__main__':
-    print_hi('PyCharm')
+# def search_koord(x):
+#     if 1 <= x <= 4:
+#         return {
+#                 x == 1: '(0 < x < +∞, 0 < y < +∞)',
+#                 x == 2: '(-∞ < x < 0, 0 < y < +∞)',
+#                 x == 3: '(-∞ < x < 0, -∞ < y < 0)',
+#                 x == 4: '(0 < x < +∞, -∞ < x < 0)'
+#                 }[True]
+# def is_int(str):
+#     try:
+#         int(str)
+#         return int(str)
+#     except ValueError:
+#         return False
+#
+# n = is_int(input('Введите четверть цифрой (Пример: 1): '))
+# if n != False:
+#     print(search_koord(n))
+# else:
+#     print('Введено не число!')
 
-# See PyCharm help at https://www.jetbrains.com/help/pycharm/
+# 5. Напишите программу, которая принимает на вход координаты двух точек и находит расстояние между ними в 2D пространстве.
+
+def size(a, b):
+    return ((a[1]-a[0])**2 + (b[1]-b[0])**2)**0.5
+
+a = [-1, 7]
+b = [7, 1]
+
+print(size(a, b))
